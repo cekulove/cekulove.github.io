464f2d9ea40afb435e8a318e011e311362812820
cekulove <1046782978@qq.com>
2017-03-27 23:57:00 +0800

    webgl

--- _site/AAAA/webGL/test_1.js
+++ _site/AAAA/webGL/test_1.js
@@ -5,44 +5,44 @@ function test1(){
 	canvas.width="1000";
 	canvas.height="1000";
 	canvas.style.border="1px solid black";
-var ctx = canvas.getContext("webgl");
-document.getElementById("view").appendChild(canvas);	
+	var gl = canvas.getContext("webgl");
+	document.getElementById("view").appendChild(canvas);	
 				
 		
-var VSHADER_SOURCE=
-'	void main(){					'+
-'		gl_Position=vec4(0.0,0.0,0.0,1.0);	'+
-'		gl_PointSize=10.0;			'+
-'	}						';
+	var VSHADER_SOURCE=
+	'	void main(){							'+
+	'		gl_Position=vec4(0.0,0.0,0.0,1.0);	'+
+	'		gl_PointSize=10.0;					'+
+	'	}										';
 
-var FSHADER_SOURCE=
-'	void main(){					'+
-'		gl_FragColor=vec4(1.0,0.0,0.0,1.0);	'+
-'	}						';
-var vertexShader=ctx.createShader(ctx.VERTEX_SHADER);
-ctx.shaderSource(vertexShader,VSHADER_SOURCE);
-ctx.compileShader(vertexShader);
+	var FSHADER_SOURCE=
+	'	void main(){							'+
+	'		gl_FragColor=vec4(1.0,0.0,0.0,1.0);	'+
+	'	}										';
+	
+	var vertexShader=gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertexShader,VSHADER_SOURCE);
+	gl.compileShader(vertexShader);
 
-var fragmentShader=ctx.createShader(ctx.FRAGMENT_SHADER);
-ctx.shaderSource(fragmentShader,FSHADER_SOURCE);
-ctx.compileShader(fragmentShader);
+	var fragmentShader=gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragmentShader,FSHADER_SOURCE);
+	gl.compileShader(fragmentShader);
 
-var program=ctx.createProgram();
+	var program=gl.createProgram();
+	gl.attachShader(program,vertexShader);
+	gl.attachShader(program,fragmentShader);
+	gl.linkProgram(program);
 
-ctx.attachShader(program,vertexShader);
-ctx.attachShader(program,fragmentShader);
-ctx.linkProgram(program);
+	if(!gl.getProgramParameter(program,gl.LINK_STATUS)){
+		var info = gl.getProgramInfoLog(program);
+		throw "Could not compile WebGL program.";
+	}
 
-if(!ctx.getProgramParameter(program,ctx.LINK_STATUS)){
-	var info = ctx.getProgramInfoLog(program);
-	throw "Could not compile WebGL program.";
-}
+	gl.useProgram(program);
 
-ctx.useProgram(program);
+	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.clear(gl.COLOR_BUFFER_BIT);
+	gl.drawArrays(gl.PONTS,0,1);
 
-ctx.clearColor(0.0,0.0,0.0,1.0);
-ctx.clear(ctx.COLOR_BUFFER_BIT);
-ctx.drawArrays(ctx.PONTS,0,1);
-
-ctx.deleteProgram(program);
+	gl.deleteProgram(program);
 }
