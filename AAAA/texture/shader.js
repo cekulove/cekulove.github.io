1221c4ef684fb9e0e734ce10bb7a200e394498b7
cekulove <1046782978@qq.com>
2017-05-17 15:31:46 +0800

    buxie

--- AAAA/texture/shader.js
+++ AAAA/texture/shader.js
@@ -1,16 +1,22 @@
 // Shader Source
 var VSHADER_SOURCE =
-'	attribute vec2 a_Position;		'+
-'	attribute vec2 a_TexCoord;		'+
-'	varying vec2 v_TexCoord;				'+
-'	void main(){								      '+
-'		v_TexCoord=a_TexCoord;				'+
-'		gl_Position = vec4(a_Position,0,1);		'+
-'	}											';
+'	attribute vec2 a_Position;						'+
+'	attribute vec2 a_TexCoord;						'+
+'	varying vec2 v_TexCoord;						'+
+'	void main(){									'+
+'		v_TexCoord=a_TexCoord;						'+
+'		gl_Position = vec4(a_Position,0,1);			'+
+'	}												';
 
 var FSHADER_SOURCE =
-'	uniform highp sampler2D u_Sampler;  '+
-'	varying highp vec2 v_TexCoord; '+
-'	void main(){								'+
-'		gl_FragColor = texture2D(u_Sampler,v_TexCoord);					'+
-'	}											';
+'	precision highp float;															'+
+'	uniform sampler2D u_Sampler;  													'+
+'	varying vec2 v_TexCoord; 														'+
+'	void main(){																	'+
+'	float alpha=texture2D(u_Sampler,v_TexCoord).a;									'+
+'	float grey=dot(texture2D(u_Sampler,v_TexCoord).rgb,vec3(0.299,0.587,0.114));	'+
+'	if (gl_FragCoord.x<525.0)														'+
+'	   	gl_FragColor = vec4(grey,grey,grey,alpha);									'+
+'	else																			'+
+'		gl_FragColor = texture2D(u_Sampler,v_TexCoord);								'+
+'	}																				';
